7f38aa9cf69c4795d23db04347f9d665c0fa0add
aldotestino <aldo.testino@libero.it>
2022-01-28 16:52:02 +0100

    json lower case properties

--- src/models/book.go
+++ src/models/book.go
@@ -5,7 +5,7 @@ import "github.com/uptrace/bun"
 type Book struct {
 	bun.BaseModel `bun:"table:books,alias:b"`
 
-	ID     int64  `bun:"id,pk,autoincrement"`
-	Title  string `bun:"title,notnull"`
-	Author string `bun:"author,notnull"`
+	ID     int64  `bun:"id,pk,autoincrement" json:"id"`
+	Title  string `bun:"title,notnull" json:"title"`
+	Author string `bun:"author,notnull" json:"author"`
 }
